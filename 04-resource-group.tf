9365e36384352d6608e046d1bf7f3bb20c1f6cae
lexington Johnson <52765566+notgnixel@users.noreply.github.com>
2022-06-06 10:26:07 -0400

    updated syntax for tags

--- 04-resource-group.tf
+++ 04-resource-group.tf
@@ -3,5 +3,7 @@ resource "azurerm_resource_group" "wordpress-rg" {
   # name = "${local.resource_name_prefix}-${var.resource_group_name}"
   name = "${var.resource_group_name}"
   location = var.resource_group_location
-  tags = "wordpress"
+  tags = {
+    "image" = "wordpress"
+    }
 }
