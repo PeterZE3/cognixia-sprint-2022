445ec58c09b15fbe0106e8ffda9dcbf9f2d75064
lexington Johnson <52765566+notgnixel@users.noreply.github.com>
2022-06-06 14:39:56 -0400

    added comments to storage-account

--- storage-account.tf
+++ storage-account.tf
@@ -1,3 +1,4 @@
+# Terraform configuration
 terraform {
   required_providers {
     azurerm = {
@@ -12,22 +13,27 @@ resource "azurerm_resource_group" "{ResourceGroupName}" {
   name = "{ResourceGroupName}"
   location = "centralus"
 }
-
+# Config Storage account
 resource "azurerm_storage_account" "{StorageAccountName}" {
   name                     = "{StorageAccountName}"
   resource_group_name      = azurerm_resource_group.{ResourceGroup}.name
   location                 = azurerm_resource_group.{ResourceGroup}.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "LRS" # Locally Redundant Storage(backup storage)
 }
 
-resource "azurerm_storage_container" "{Blob}" {
-  name                  = "{Blob}"
+resource "azurerm_storage_container" "{ContainerName}" {
+  name                  = "{logs}"
   storage_account_name  = azurerm_storage_account.{StorageAccountName}.name
   container_access_type = "private"
 }
-
-resource "azurerm_storage_queue" "{Queue}" {
-  name                 = "{Queue}"
+/**
+* Azure Queue Storage is a service for storing large numbers/messages, enables
+* communication between componenets of a distibuted app. Each queue maintains a
+* list of messages that can be added by a sender component and processed by a
+* reciever component.
+*/
+resource "azurerm_storage_queue" "{QueueName}" {
+  name                 = "{messasges}"
   storage_account_name = azurerm_storage_account.{StorageAccountName}.name
 }
