fc64a3aea19c94046aa969ec0af3e98fd8b3746d
lexington Johnson <52765566+notgnixel@users.noreply.github.com>
2022-06-06 11:53:17 -0400

    edited files

--- 04-resource-group.tf
+++ 04-resource-group.tf
@@ -1,4 +1,4 @@
-# Resource-1: Azure Resource Group
+# Word press resource group
 resource "azurerm_resource_group" "wordpress-rg" {
   # name = "${local.resource_name_prefix}-${var.resource_group_name}"
   name = "${var.resource_group_name}"
