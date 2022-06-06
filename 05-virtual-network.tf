076354d030d5d719b54a76623e7e668ba52fc2d3
lexington Johnson <52765566+notgnixel@users.noreply.github.com>
2022-06-06 18:09:32 -0400

    vnet works

--- 05-virtual-network.tf
+++ 05-virtual-network.tf
@@ -1,42 +1,23 @@
 #  NSG for vnet
 resource "azurerm_network_security_group" "security-group" {
   name                = "security-group"
-  location            = azurerm_resource_group.wordpress-rg.location
-  resource_group_name = azurerm_resource_group.wordpress-rg.name
+  location            = var.rglocation
+  resource_group_name = "mysql-rg"
 }
 #   virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "virtual-network"
-  location            = azurerm_resource_group.wordpress-rg.location
-  resource_group_name = azurerm_resource_group.wordpress-rg.name
+  location            = var.rglocation
+  resource_group_name = "mysql-rg"
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+# Subnet
+subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
 }
-
-# Resource-1: Create WebTier Subnet
-resource "azurerm_subnet" "websubnet" {
-  name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.web_subnet_address  
-}
-
-# Resource-2: Create Network Security Group (NSG)
-resource "azurerm_network_security_group" "web_subnet_nsg" {
-  name                = "${azurerm_subnet.websubnet.name}-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Resource-3: Associate NSG and Subnet
-resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
-  depends_on = [ azurerm_network_security_rule.web_nsg_rule_inbound] # Every NSG Rule Association will disassociate NSG from Subnet and Associate it, so we associate it only after NSG is completely created - Azure Provider Bug https://github.com/terraform-providers/terraform-provider-azurerm/issues/354  
-  subnet_id                 = azurerm_subnet.websubnet.id
-  network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
-}
-
   ## Locals Block for Security Rules
-locals {
+/*locals {
   web_inbound_ports_map = {
     "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
     "110" : "443",
@@ -57,4 +38,4 @@ resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
-}
+}*/
