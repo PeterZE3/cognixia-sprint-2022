a4cc2424efdd2e7a4145ff58630949087cfc6f33
PeterZE3 <103215360+PeterZE3@users.noreply.github.com>
2022-06-07 11:31:26 -0400

    uploading TF code for VM

--- 05-virtual-network.tf
+++ 05-virtual-network.tf
@@ -1,23 +1,42 @@
 #  NSG for vnet
 resource "azurerm_network_security_group" "security-group" {
   name                = "security-group"
-  location            = var.rglocation
-  resource_group_name = "mysql-rg"
+  location            = azurerm_resource_group.wordpress-rg.location
+  resource_group_name = azurerm_resource_group.wordpress-rg.name
 }
 #   virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "virtual-network"
-  location            = var.rglocation
-  resource_group_name = "mysql-rg"
+  location            = azurerm_resource_group.wordpress-rg.location
+  resource_group_name = azurerm_resource_group.wordpress-rg.name
   address_space       = ["10.0.0.0/16"]
-# Subnet
-subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
+
+# Resource-1: Create WebTier Subnet
+resource "azurerm_subnet" "websubnet" {
+  name                 = "wordpress-subnet"
+  resource_group_name  = azurerm_resource_group.wordpress-rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.web_subnet_address  
+}
+
+# Resource-2: Create Network Security Group (NSG)
+resource "azurerm_network_security_group" "web_subnet_nsg" {
+  name                = "${azurerm_subnet.websubnet.name}-nsg"
+  location            = azurerm_resource_group.wordpress-rg.location
+  resource_group_name = azurerm_resource_group.wordpress-rg.name
+}
+
+# Resource-3: Associate NSG and Subnet
+resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
+  depends_on = [ azurerm_network_security_rule.web_nsg_rule_inbound] # Every NSG Rule Association will disassociate NSG from Subnet and Associate it, so we associate it only after NSG is completely created - Azure Provider Bug https://github.com/terraform-providers/terraform-provider-azurerm/issues/354  
+  subnet_id                 = azurerm_subnet.websubnet.id
+  network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
+}
+
   ## Locals Block for Security Rules
-/*locals {
+locals {
   web_inbound_ports_map = {
     "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
     "110" : "443",
@@ -36,6 +55,6 @@ resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
   destination_port_range      = each.value 
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
+  resource_group_name         = azurerm_resource_group.wordpress-rg.name
   network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
-}*/
+}
---
+++ 07-wordpressvm.tf
@@ -0,0 +1,50 @@
+# Locals Block for custom data
+# locals {
+#   webvm_custom_data = <<CUSTOM_DATA{}
+# }
+
+resource "azurerm_network_interface" "web_linuxvm_nic" {
+  name = "wordpress-web-linuxvm-nic"
+  location = azurerm_resource_group.wordpress-rg.location
+  resource_group_name = azurerm_resource_group.wordpress-rg.name
+
+  ip_configuration {
+    name = "web-linuxvm-ip-1"
+    subnet_id = azurerm_subnet.websubnet.id
+    private_ip_address_allocation = "Dynamic"
+    #public_ip_address_id = azurerm_public_ip.web_linuxvm_public_ip.id
+  }
+}
+
+# Resource: Azure Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "web_linuxvm" {
+  name = "wordpress-web-linuxvm"
+  # computer_name = 
+  resource_group_name = azurerm_resource_group.wordpress-rg.name
+  location = azurerm_resource_group.wordpress-rg.location
+  size = "Standard_B1s"
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [ azurerm_network_interface.web_linuxvm_nic.id ]
+
+  admin_ssh_key {
+    username = "azureuser"
+    public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
+  }
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer = "UbuntuServer"
+    sku = "18.04-LTS"
+    version = "latest"
+  }
+
+  # custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")
+#   custom_data = base64encode(local.webvm_custom_data)
+}
