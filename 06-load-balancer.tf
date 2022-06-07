ad7b97f1c004f1635e5e61d3d7e98efc40057f91
Tiimothy Longley <99631145+t-longley@users.noreply.github.com>
2022-06-07 14:32:57 -0500

    load balancer updates

--- 06-load-balancer.tf
+++ 06-load-balancer.tf
@@ -10,6 +10,7 @@ resource "azurerm_lb" "load_balancer" {
   name                = "AzureLoadBalancer"
   location            = azurerm_resource_group.wordpress-rg.location
   resource_group_name = azurerm_resource_group.wordpress-rg.name
+ 
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
@@ -23,10 +24,44 @@ resource "azurerm_lb_backend_address_pool" "backend_address_pool" {
   name            = "BackEndAddressPool"
 }
 
-# resource "azurerm_lb_backend_address_pool_address" "backend_address_pool_address" {
-#     name = "backendPoolAddresses"
-#     backend_address_pool_id = azurerm_lb.load_balancer.id
-#     //need the vnet setup for the virtual_network_id
-#     virtual_network_id = azurerm_virtual_network.vnet.id   
-#     ip_address = "10.0.0.1"
-# }
+ resource "azurerm_lb_backend_address_pool_address" "backend_address_pool_address" {
+     name = "backendPoolAddresses"
+     backend_address_pool_id = azurerm_lb.load_balancer.id
+     //need the vnet setup for the virtual_network_id
+     virtual_network_id = azurerm_virtual_network.vnet.id   
+     ip_address = "10.0.0.1"
+ }
+
+ //NAT rules for access to the load balancer
+ resource "azurerm_lb_nat_rule" "nat_rules" {
+   resource_group_name = azurerm_resource_group.wordpress-rg.name
+   loadbalancer_id = azurerm_lb.load_balancer.id
+   name = "NAT_connection_rules"
+   protocol = "Tcp"
+   frontend_port = 8080
+   backend_port = 80
+   frontend_ip_configuration_name = azurerm_lb.load_balancer.frontend_ip_configuration[0]
+ }
+
+ //outbound rules for the load balancer
+ resource "azurerm_lb_outbound_rule" "outbound_rules" {
+   loadbalancer_id = azurerm_lb.load_balancer.id
+   name = "OutboundRule"
+   protocol = "Tcp"
+   backend_address_pool_id = azurerm_lb_backend_address_pool.backend_address_pool.id
+
+   frontend_ip_configuration {
+     name = azurerm_lb.load_balancer.frontend_ip_configuration[0]
+   }
+ }
+
+ //load blancer rules
+ resource "azurerm_lb_rule" "lb_rule" {
+   loadbalancer_id = azurerm_lb.load_balancer.id
+   name = "LoadBalancerRule"
+   protocol = "Tcp"
+   frontend_port = 80
+   backend_port = 80
+   frontend_ip_configuration_name = azurerm_lb.load_balancer.frontend_ip_configuration[0]
+   load_distribution = "Default"
+ }
